f8735a1668fd4a1f8bae1b8bd8d4b064ae9e6ce3
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-05-06 14:43:30 +0800

    Add files via upload

--- demo_fastapi/model/admins.py
+++ demo_fastapi/model/admins.py
@@ -80,7 +80,7 @@ async def admin_change_password(
 
 @Admin_Router.delete("/Deleteadmin/{admin_name}", response_model=dict)
 async def delete_admin_account(
-    admin_name: int,
+    admin_name: str = (...),
     db=Depends(get_db)
 ):
     try:
@@ -94,7 +94,7 @@ async def delete_admin_account(
             raise HTTPException(status_code=404, detail="account not found")
 
         # Delete the account
-        query_delete_account = "DELETE FROM admin_name WHERE admin_name = %s"
+        query_delete_account = "DELETE FROM admin WHERE admin_name = %s"
         cursor.execute(query_delete_account, (admin_name,))
         db.commit()
         cursor.close()
