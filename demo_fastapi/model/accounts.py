1a55be0493a104dfc0c88014dd991e5cfb2906bd
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-05-09 09:52:21 +0800

    Add files via upload

--- demo_fastapi/model/accounts.py
+++ demo_fastapi/model/accounts.py
@@ -4,7 +4,7 @@ from .db import get_db
 import bcrypt
 import mysql.connector.errors
 
-Accounts_Router = APIRouter(tags=["User Accounts"])
+Accounts_Router = APIRouter(tags=["UserAccounts"])
 
 # CRUD operations
 
@@ -15,43 +15,64 @@ def hash_password(password: str):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')  # Decode bytes to string for storage
 
+@Accounts_Router.post("/Login/", response_model=list)
+async def Login(
+    email: str = Form(...), 
+    password: str = Form(...),
+    db=Depends(get_db)):
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM users WHERE email = %s"
+    cursor.execute(query, (email,))
+    user = cursor.fetchone()
+
+    if not user or user[1] != password:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    cursor.close()
+    return {"message": "Login successful"}
+
 @Accounts_Router.get("/AllUsers/", response_model=list)
-async def get_all_users(db=Depends(get_db)):
-    query = "SELECT * FROM user_accounts"
+async def getAllUsers(db=Depends(get_db)):
+    query = "SELECT * FROM users"
     cursor = db.cursor()
     cursor.execute(query)
     user_data = [
         {
-        "id": user[0], 
-        "username": user[1], 
-        "password": user[2], 
-        "email": user[3]
+        "email": user[0], 
+        "password": user[1], 
+        "firstname": user[2], 
+        "lastname": user[3],
+        "role": user[4]
         }      
         for user in cursor.fetchall()]
     return user_data
 
 @Accounts_Router.get("/users/{user_id}", response_model=dict)
-async def read_user(
-    user_id: int, 
+async def readUser(
+    email: str, 
     db=Depends(get_db)
 ):
     try:
         cursor = db.cursor()
-        query = "SELECT id, username FROM user_accounts WHERE id = %s"
-        cursor.execute(query, (user_id,))
+        query = "SELECT email, firstname, lastname FROM users WHERE email = %s"
+        cursor.execute(query, (email,))
         user = cursor.fetchone()
         if user is not None:
-            return {"id": user[0], "username": user[1]}
+            return {"email": user[0], "firstname": user[1], "lastname": user[2]}
         raise HTTPException(status_code=404, detail="User not found")
     finally:
         if cursor:
             cursor.close()  
 
 @Accounts_Router.post("/CreateAccount/", response_model=dict)
-async def create_user(
+async def createUser(
     email: str = Form(...), 
-    username: str = Form(...), 
     password: str = Form(...), 
+    firstname: str = Form(...), 
+    lastname: str = Form(...), 
+    role: str = "visitor",
     db=Depends(get_db)
 ):
     cursor = None
@@ -60,23 +81,18 @@ async def create_user(
         hashed_password = hash_password(password)
         
         # Check if the account exists
-        query_check_existing = "SELECT id FROM user_accounts WHERE email = %s OR username = %s"
-        cursor.execute(query_check_existing, (email, username,))
+        query_check_existing = "SELECT email FROM users WHERE email = %s"
+        cursor.execute(query_check_existing, (email,))
         existing_account = cursor.fetchone()
         
-        query = "INSERT INTO user_accounts (email, username, password) VALUES (%s, %s, %s)"
-        cursor.execute(query, (email, username, hashed_password))
+        query = "INSERT INTO users (email, password, firstname, lastname, role) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (email, password, firstname, lastname, role)) # Temporarily not hashed
         existing_account = cursor.fetchone()
 
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        new_user_id = cursor.fetchone()[0]
         db.commit()
         cursor.close()
         
-        return {"Message": "Account created successfully!",
-            "id": new_user_id,
-            "email": email,
-            "username": username}
+        return {"Message": "Account created successfully!"}
     
     except mysql.connector.errors.IntegrityError as e:
         if e.errno == 1062:  # Duplicate entry error
@@ -89,21 +105,18 @@ async def create_user(
             cursor.close()  
 
 @Accounts_Router.put("/users/{user_id}", response_model=dict)
-async def user_change_password(
-    user_id: int,
+async def Change_Pass(
+    email: str,
     password: str = Form(...),
     db=Depends(get_db)
 ):
     try:    
         cursor = db.cursor()
-        # Hash the password using bcrypt
         hashed_password = hash_password(password)
 
-        # Update user information in the database 
-        query = "UPDATE user_accounts SET password = %s WHERE id = %s"
-        cursor.execute(query, (hashed_password, user_id))
+        query = "UPDATE users SET password = %s WHERE email = %s"
+        cursor.execute(query, (password, email)) # Temporarily not hashed
 
-        # Check if the update was successful
         if cursor.rowcount > 0:
             db.commit()
             return {"message": "User password successfully"}
@@ -116,24 +129,24 @@ async def user_change_password(
             cursor.close()  
 
 @Accounts_Router.delete("/DeleteAccount/{account_id}", response_model=dict)
-async def delete_account(
-    account_id: int,
+async def deleteAccount(
+    email: str,
     db=Depends(get_db)
 ):
     try:
         cursor = db.cursor()
 
         # Check if the account exists
-        query_check_account = "SELECT id FROM user_accounts WHERE id = %s"
-        cursor.execute(query_check_account, (account_id,))
+        query_check_account = "SELECT email FROM users WHERE email = %s"
+        cursor.execute(query_check_account, (email,))
         existing_account = cursor.fetchone()
 
         if not existing_account:
             raise HTTPException(status_code=404, detail="Account not found")
 
         # Delete the account
-        query_delete_account = "DELETE FROM user_accounts WHERE id = %s"
-        cursor.execute(query_delete_account, (account_id,))
+        query_delete_account = "DELETE FROM users WHERE email = %s"
+        cursor.execute(query_delete_account, (email,))
         db.commit()
         cursor.close()
 
