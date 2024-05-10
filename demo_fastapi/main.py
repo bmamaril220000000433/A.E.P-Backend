df0586af63d8cd0176a39fa257134687807bd437
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-05-10 11:37:22 +0800

    Update main.py

--- demo_fastapi/main.py
+++ demo_fastapi/main.py
@@ -4,22 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from model.visitor import Visitor_Router
 from model.accounts import Accounts_Router
 from model.guards import Guards_Router
-from model.admins import Admin_Router
 from model.office import Office_Router
 
 app = FastAPI()
-
-origins = {
-    "http://localhost:",
-    "http://localhost:5173"
-}
 
 # Include CRUD routes from users module
 app.include_router(Visitor_Router, prefix="/api")
 app.include_router(Accounts_Router, prefix="/api")
 app.include_router(Guards_Router, prefix="/api")
-app.include_router(Admin_Router, prefix="/api")
 app.include_router(Office_Router, prefix="/api")
+
+origins = [
+    "http://localhost:",
+    "http://localhost:5173"
+]
 
 app.add_middleware(
     CORSMiddleware,
