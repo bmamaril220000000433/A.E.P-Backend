159a09af2d5027ce3ec2804aa1b783e805fbad7a
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-05-14 23:11:30 +0800

    Add files via upload

--- entrypass.sql
+++ entrypass.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 03:02 PM
+-- Generation Time: May 14, 2024 at 05:11 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -68,6 +68,7 @@ CREATE TABLE `office` (
 --
 
 INSERT INTO `office` (`office_name`, `office_in_charge`) VALUES
+('Bookstore', 'George'),
 ('Cashier', 'Blair'),
 ('OSAD', 'Clara'),
 ('Registrar', 'Abby');
@@ -94,7 +95,7 @@ CREATE TABLE `users` (
   `password` varchar(50) NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
-  `role` varchar(20) NOT NULL
+  `role` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -102,10 +103,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`email`, `password`, `firstname`, `lastname`, `role`) VALUES
-('Person1@gmail.com', 'Person11111', 'Person11', 'Person11', 'visitor'),
-('Person2@gmail.com', 'Person2', 'Person22', 'Person22', 'visitor'),
-('Person3@gmail.com', 'Person3', 'Person33', 'Person33', 'visitor'),
-('test', 'test', 'test', 'test', 'visitor');
+('test1', 'test1', 'Reag', 'Paddle', 'visitor'),
+('test2', 'test2', 'Stardew', 'Ravago', 'visitor');
 
 -- --------------------------------------------------------
 
@@ -114,24 +113,26 @@ INSERT INTO `users` (`email`, `password`, `firstname`, `lastname`, `role`) VALUE
 --
 
 CREATE TABLE `visitor` (
-  `visitor_id` int(11) NOT NULL,
-  `visitor_fname` varchar(30) NOT NULL,
-  `visitor_lname` varchar(30) NOT NULL,
-  `purpose` varchar(200) NOT NULL,
+  `transaction_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `date_of_visit` varchar(10) NOT NULL,
-  `time_of_visit` varchar(7) NOT NULL
+  `time_of_visit` varchar(10) NOT NULL,
+  `office_name` varchar(20) NOT NULL,
+  `purpose` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `visitor`
 --
 
-INSERT INTO `visitor` (`visitor_id`, `visitor_fname`, `visitor_lname`, `purpose`, `date_of_visit`, `time_of_visit`) VALUES
-(1, 'Person1', 'Person1', 'Cashier', '2024-04-15', '10:00am'),
-(2, 'Person2', 'Person2', 'Registrar', '2024-04-15', '11:00am'),
-(3, 'Person3', 'Person3', 'Registrar', '2024-04-15', '1:00pm'),
-(4, 'Person4', 'Person4', 'Cashier', '2024-04-15', '2:00:pm'),
-(5, 'Person5', 'Person5', 'OSAD', '2024-04-15', '2:00pm');
+INSERT INTO `visitor` (`transaction_id`, `email`, `date_of_visit`, `time_of_visit`, `office_name`, `purpose`) VALUES
+(1, 'test1', '2024-05-15', '02:14', 'Cashier', 'Im going to cashier'),
+(3, 'test1', '2024-05-15', '02:14', 'OSAD', 'Im going to OSAD'),
+(4, 'test2', '2024-05-15', '02:14', 'Registrar', 'Im going to register'),
+(5, 'test2', '2024-05-15', '02:14', 'Bookstore', 'Im going to bookstore'),
+(8, 'test2', '2024-05-14', '05:39', 'Bookstore', 'book'),
+(10, 'test1', '2024-05-15', '2:14', 'OSAD', 't'),
+(11, 'test1', '2024-05-15', '21:44', 'Cashier', 'im going to pay');
 
 -- --------------------------------------------------------
 
@@ -141,38 +142,12 @@ INSERT INTO `visitor` (`visitor_id`, `visitor_fname`, `visitor_lname`, `purpose`
 
 CREATE TABLE `visit_transaction` (
   `transaction_number` int(11) NOT NULL,
-  `visitor_id` int(11) NOT NULL,
-  `visitor_fname` varchar(30) NOT NULL,
-  `visitor_lname` varchar(30) NOT NULL,
-  `purpose` varchar(200) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
   `guard_name` varchar(30) DEFAULT NULL,
   `office_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `visit_transaction_info`
--- (See below for the actual view)
---
-CREATE TABLE `visit_transaction_info` (
-`transaction_number` int(11)
-,`visitor_id` int(11)
-,`visitor_fname` varchar(30)
-,`visitor_lname` varchar(30)
-,`purpose` varchar(200)
-,`guard_name` varchar(30)
-,`office_name` varchar(20)
-);
-
--- --------------------------------------------------------
-
---
--- Structure for view `visit_transaction_info`
---
-DROP TABLE IF EXISTS `visit_transaction_info`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `visit_transaction_info`  AS SELECT `vt`.`transaction_number` AS `transaction_number`, `v`.`visitor_id` AS `visitor_id`, `v`.`visitor_fname` AS `visitor_fname`, `v`.`visitor_lname` AS `visitor_lname`, `v`.`purpose` AS `purpose`, `g`.`guard_name` AS `guard_name`, `o`.`office_name` AS `office_name` FROM (((`visit_transaction` `vt` join `visitor` `v` on(`vt`.`visitor_id` = `v`.`visitor_id`)) join `guard` `g` on(`vt`.`guard_name` = `g`.`guard_name`)) join `office` `o` on(`vt`.`office_name` = `o`.`office_name`)) ;
 
 --
 -- Indexes for dumped tables
@@ -208,22 +183,26 @@ ALTER TABLE `specific_office`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `idx_email` (`email`);
 
 --
 -- Indexes for table `visitor`
 --
 ALTER TABLE `visitor`
-  ADD PRIMARY KEY (`visitor_id`);
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `fk_user_email` (`email`),
+  ADD KEY `fk_office_office_name` (`office_name`);
 
 --
 -- Indexes for table `visit_transaction`
 --
 ALTER TABLE `visit_transaction`
   ADD PRIMARY KEY (`transaction_number`),
-  ADD KEY `fk_visitor_id` (`visitor_id`),
   ADD KEY `fk_guard_name` (`guard_name`),
-  ADD KEY `fk_office_name` (`office_name`);
+  ADD KEY `fk_office_name` (`office_name`),
+  ADD KEY `fk_user_email1` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -233,7 +212,7 @@ ALTER TABLE `visit_transaction`
 -- AUTO_INCREMENT for table `visitor`
 --
 ALTER TABLE `visitor`
-  MODIFY `visitor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `visit_transaction`
@@ -260,12 +239,19 @@ ALTER TABLE `specific_office`
   ADD CONSTRAINT `fk_transaction_number` FOREIGN KEY (`transaction_number`) REFERENCES `visit_transaction` (`transaction_number`);
 
 --
+-- Constraints for table `visitor`
+--
+ALTER TABLE `visitor`
+  ADD CONSTRAINT `fk_office_office_name` FOREIGN KEY (`office_name`) REFERENCES `office` (`office_name`),
+  ADD CONSTRAINT `fk_user_email` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
+
+--
 -- Constraints for table `visit_transaction`
 --
 ALTER TABLE `visit_transaction`
   ADD CONSTRAINT `fk_guard_name` FOREIGN KEY (`guard_name`) REFERENCES `guard` (`guard_name`),
   ADD CONSTRAINT `fk_office_name` FOREIGN KEY (`office_name`) REFERENCES `office` (`office_name`),
-  ADD CONSTRAINT `fk_visitor_id` FOREIGN KEY (`visitor_id`) REFERENCES `visitor` (`visitor_id`);
+  ADD CONSTRAINT `fk_user_email1` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
